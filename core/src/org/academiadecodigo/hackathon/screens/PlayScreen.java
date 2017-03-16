c683114348eb389d396ea0a2348b712f6a47ff00
Joaquim T. Duarte <joaquimtd.info@gmail.com>
2017-03-16 16:23:17 +0000

    Added some boilerplate to PlayScreen

--- core/src/org/academiadecodigo/hackathon/screens/PlayScreen.java
+++ core/src/org/academiadecodigo/hackathon/screens/PlayScreen.java
@@ -1,7 +1,60 @@
 package org.academiadecodigo.hackathon.screens;
 
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import org.academiadecodigo.hackathon.Indiana;
+
 /**
  * Created by codecadet on 16/03/17.
  */
 public class PlayScreen extends AbstractGameScreen {
+
+    // BOX2D
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
+    // TILED MAP    TODO: Está aqui preparado para ti BONI
+//    private TmxMapLoader mapLoader;
+//    private TiledMap map;
+//    private OrthogonalTiledMapRenderer renderer;
+
+    public PlayScreen(Indiana game) {
+
+        //this.game = game;
+        // BOX2D
+        world = new World(new Vector2(0, 0), true);
+        b2dr = new Box2DDebugRenderer();
+
+        // TILED MAP        TODO: Está aqui preparado para ti BONI
+//        mapLoader = new TmxMapLoader();
+//        map = mapLoader.load("map.tmx");
+//        renderer = new OrthogonalTiledMapRenderer(map);
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        // CREATE THE MAP OBJECTS ON LAYER 2
+        for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+
+            Rectangle rect = ((RectangleMapObject)object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / Indiana.PPM, (rect.getY() + rect.getHeight() / 2) / Indiana.PPM);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+    }
+
 }
