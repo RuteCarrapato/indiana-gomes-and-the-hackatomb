87d60adc244c015163eee2879241649c4e9bf929
Joaquim T. Duarte <joaquimtd.info@gmail.com>
2017-03-16 17:55:45 +0000

    Defined body of Player

--- core/src/org/academiadecodigo/hackathon/gameobjects/Player.java
+++ core/src/org/academiadecodigo/hackathon/gameobjects/Player.java
@@ -1,12 +1,34 @@
 package org.academiadecodigo.hackathon.gameobjects;
 
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
 /**
  * Created by codecadet on 16/03/17.
  */
 public class Player  extends GameObject {
 
-    public Player() {
+    public Player(World world) {
 
+        super(world);
+        definePlayer();
+    }
+
+    public void definePlayer() {
+
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(32, 32);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        b2dbody = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(5);
+
+        fdef.shape = shape;
+        b2dbody.createFixture(fdef);
     }
 
 }
