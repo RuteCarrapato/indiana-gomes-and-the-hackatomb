4f6e23b49cd259477c0580b7a5141228f8485171
Ruben Mateus <ruben.mateus3@gmail.com>
2017-03-16 16:41:08 +0000

    added PlayScreen and camera with FitViewPort

--- core/src/org/academiadecodigo/hackathon/screens/AbstractGameScreen.java
+++ core/src/org/academiadecodigo/hackathon/screens/AbstractGameScreen.java
@@ -1,7 +1,10 @@
 package org.academiadecodigo.hackathon.screens;
 
+import com.badlogic.gdx.Screen;
+
 /**
  * Created by codecadet on 16/03/17.
  */
-public abstract class AbstractGameScreen {
+public abstract class AbstractGameScreen implements Screen {
+
 }
--- core/src/org/academiadecodigo/hackathon/screens/PlayScreen.java
+++ core/src/org/academiadecodigo/hackathon/screens/PlayScreen.java
@@ -1,7 +1,70 @@
 package org.academiadecodigo.hackathon.screens;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import org.academiadecodigo.hackathon.Indiana;
+
 /**
  * Created by codecadet on 16/03/17.
  */
 public class PlayScreen extends AbstractGameScreen {
+
+    private Indiana game;
+    private Texture texture;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+
+
+    public PlayScreen(Indiana game) {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(800, 480, gameCam);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
+        game.batch.begin();
+        game.batch.draw(texture, 50, 50);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
