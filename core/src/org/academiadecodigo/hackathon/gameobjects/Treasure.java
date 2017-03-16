f73412612324b4213090f0d5a3dae34943303f17
Joaquim T. Duarte <joaquimtd.info@gmail.com>
2017-03-16 17:58:34 +0000

    Added default gameobject constructors

--- core/src/org/academiadecodigo/hackathon/gameobjects/Treasure.java
+++ core/src/org/academiadecodigo/hackathon/gameobjects/Treasure.java
@@ -1,7 +1,13 @@
 package org.academiadecodigo.hackathon.gameobjects;
 
+import com.badlogic.gdx.physics.box2d.World;
+
 /**
  * Created by codecadet on 16/03/17.
  */
 public class Treasure extends GameObject {
+
+    public Treasure(World world) {
+        super(world);
+    }
 }
