83703393dc7d12df834922a1c2d891553cb77b35
Joaquim T. Duarte <joaquimtd.info@gmail.com>
2017-03-16 15:21:41 +0000

    initial commit

---
+++ desktop/src/org/academiadecodigo/hackathon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.hackathon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.hackathon.Indiana;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Indiana(), config);
+	}
+}
