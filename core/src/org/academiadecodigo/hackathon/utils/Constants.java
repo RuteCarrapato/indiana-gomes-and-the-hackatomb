37f3a7535632cff224ebc619428e3a3a201a08e5
Veronica M Corrales C <veronica.corrales.c@gmail.com>
2017-03-17 04:13:31 +0000

    Implementing methods in Projectile class

--- core/src/org/academiadecodigo/hackathon/utils/Constants.java
+++ core/src/org/academiadecodigo/hackathon/utils/Constants.java
@@ -26,4 +26,11 @@ public class Constants {
     public static final float PLAYER_X_SPEED = 2;
 
     public static final String PLAYER_REGION_STRING = "player";
+
+    public static final String PROJECTILE_RIGHT = "bullet_right.png";
+    public static final String PROJECTILE_LEFT = "bullet_left.png";
+
+    public static final float PROJECTILE_HEIGHT = 16; // Pixels
+    public static final float PROJECTILE_WIDTH = 16; // Pixels
+
 }
